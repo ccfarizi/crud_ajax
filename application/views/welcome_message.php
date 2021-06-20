d91180dbeaf7c744378ac85cdbb05b5415acac07
Alfarizi <ccfarizi@gmail.com>
2021-06-20 23:55:20 +0700

    Change Title

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <title>Hello, world!</title>
+    <title>CRUD CI WITH AJAX</title>
   </head>
   <body>
     <div class="container">
